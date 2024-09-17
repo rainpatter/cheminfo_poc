9e9403ded4897e43c8a6b44a938c4ee5c8432e28
georgia-a-scott <georgiaxscott@gmail.com>
2024-09-17 14:57:51 +1000

    basic routing

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
 
-import test_mod
+import data_functions
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html', name=test_mod.test_variable)
+    return render_template('index.html')
 
 @app.route("/ames")
 def ames_data():
-    return render_template('ames_index.html', tables=[test_mod.df_html], titles=test_mod.col_vals)
+    return render_template('ames_index.html', json=[data_functions.df_json], tables=[data_functions.df_html], titles=data_functions.col_vals)
 
-print(test_mod.df_html)
+@app.route("/ames/<chem_id>")
+def show_ames_data(chem_id):
+    print(type(chem_id))
+    chem_dict = data_functions.match_on_id(data_functions.df_json, chem_id)
+    return render_template('ames_show.html', id=chem_id, dict=chem_dict)
 
 if __name__ == "__main__":
     app.run()
--- test_mod.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data/test_data.csv')
-df_html = df.to_html(classes="data")
-col_vals = df.columns.values
-
-test_variable = "new test"
