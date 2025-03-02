9c95f3386016fd96e65363a7e62292297ff42959
georgia-a-scott <georgiaxscott@gmail.com>
2025-03-03 02:57:51 +1100

    update

--- targeted_risk_assessment/calculator.py
+++ targeted_risk_assessment/calculator.py
@@ -5,89 +5,12 @@ import pprint
 
 # input file from ECETOC to calculate lookup values
 
-file = 'ECETOC-TRAworker-version3.2-final.XLSX - TRAlookup.csv'
+file = 'data/ECETOC-TRAworker-version3.2-final.XLSX - TRAlookup.csv'
 
 df = pd.read_csv(
     file)
 
 np.set_printoptions(legacy='1.25')
-
-# ------------------------------------------------------------------- #
-
-# CHANGE DICTIONARY VALUES HERE
-# - dictionary below called 'example_user_inputs' is for calculation - constraints are detailed below in comments
-# - if there is a list of options, please copy-paste the text string exactly as it is shown into the dictionary
-# - once you have changed the values in the dictionary, please run the program
-
-example_user_inputs = {
-    # any substance name
-    'substance_name': 'ethanol',
-    # any cas number
-    'cas_number': '64-17-5',
-    # any decimal - g/mol
-    'mol_weight': 46.069,
-    # any decimal - DNEL or OEL (mg/m3)
-    'long_term_inhalation': 10,
-    # any decimal - DNEL or OEL (mg/kg/day)
-    'long_term_dermal': 10,
-    # any decimal - DNEL or OEL (mg/m3)
-    'short_term_inhalation': 10,
-    # any decimal - DNEL or OEL (ug/cm2)
-    'local_dermal': 10,
-    # any decimal - Pascal
-    'vap_pressure_at_operating_temp': 7832.4225,
-    # can be PROC1 to PROC25
-    'proc': 'PROC7',
-    # can be 'ind' or 'prof'
-    'ind_prof': 'ind',
-    # can be 'solid' or 'liquid'
-    'phys_state': 'liquid',
-    # can be:
-    # - 'very low'
-    # - 'low'
-    # - 'medium'
-    # - 'high'
-    'fugacity': 'very low',
-    # can be:
-    # - 'outdoors'
-    # - 'indoors - no or basic ventilation'
-    # - 'indoors - good ventilation'
-    # - 'indoors - enhanced ventilation'
-    'ventilation': 'indoors - no or basic ventilation',
-    # can be:
-    # - '<15min'
-    # - '15min-1hr'
-    # - '1-4hr'
-    # - '>4hr'
-    'duration': '>4hr',
-    # can be:
-    # - '<1%'
-    # - '1-5%'
-    # - '5-25%'
-    # - '>25%'
-    'concentration': '>25%',
-    # can be:
-    #  - 'yes'
-    #  - 'no'
-    'lev': 'no',
-    # can be:
-    #  - 'no RPE'
-    #  - 'RPE90%'
-    #  - 'RPE95%'
-    'rpe_mask': 'no RPE',
-    # can be:
-    # - 'no PPE'
-    # - 'PPE80%'
-    # - 'PPE90%'
-    # - 'PPE95%'
-    'ppe_gloves': 'no PPE',
-    # can be:
-    # - 'yes'
-    # - 'no'
-    'lev_dermal': 'no'
-}
-
-# ------------------------------------------------------------------- #
 
 
 def calc_fugacity_band(dict):
@@ -449,7 +372,3 @@ def calculate_all(dict):
     return dict
 
 
-calculated_dict = calculate_all(example_user_inputs)
-
-for key, value in calculated_dict.items():
-    print(f'{key}: {value}')
